55fe9f6db28a350d14e4fca0b7cd49607aafed73
jongseo <jong980812@khu.ac.kr>
2024-08-28 19:44:12 +0900

    spatio concept 추가

--- ssv2_lavila_vmae.sh
+++ ssv2_lavila_vmae.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name example_selection_test
+#SBATCH --job-name spatio_origin_ssv2
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=12
 #SBATCH --mem-per-gpu=30G
 #SBATCH --time 1-00:00:0
 #SBATCH --partition batch_ce_ugrad
 #SBATCH -w moana-y3
-#SBATCH -o /data/jong980812/project/Video-CBM/results/ssv2/finetune_internvid_ost_temporal/%A-%x.out
-#SBATCH -e /data/jong980812/project/Video-CBM/results/ssv2/finetune_internvid_ost_temporal/%A-%x.err
+#SBATCH -o /data/jong980812/project/Video-CBM/results/origin_ssv2/finetune_internvid_ost_spatio_temporal/%A-%x.out
+#SBATCH -e /data/jong980812/project/Video-CBM/results/origin_ssv2/finetune_internvid_ost_spatio_temporal/%A-%x.err
 
-OUTPUT_DIR='/data/jong980812/project/Video-CBM/results/ssv2/finetune_internvid_ost_temporal'
+OUTPUT_DIR='/data/jong980812/project/Video-CBM/results/origin_ssv2/finetune_internvid_ost_spatio_temporal'
 
 
 python /data/jong980812/project/Video-CBM/train_video_cbm.py \
---data_set mini-SSV2 \
---nb_classes 87 \
---concept_set /data/jong980812/project/Video-CBM/data/concept_sets/ssv2_ost_temporal_concepts.txt \
+--data_set SSV2 \
+--nb_classes 174 \
+--concept_set /data/jong980812/project/Video-CBM/data/concept_sets/ssv2_ost_spatio_temporal_concepts.txt \
 --batch_size 32 \
---video-anno-path data/video_annotation/mini-ssv2 \
+--video-anno-path data/video_annotation/ssv2 \
 --data_path /local_datasets/something-something/something-something-v2-mp4 \
---print \
 --activation_dir ${OUTPUT_DIR}/activation \
 --save_dir ${OUTPUT_DIR}/model \
 --proj_steps 2000 \
--- train_video_cbm_space_time.py
+++ train_video_cbm_space_time.py
@@ -242,8 +242,8 @@ def train_cbm_and_save(args):
                                             args.feature_layer,d_train, args.concept_set, "avg", args.activation_dir)
     val_target_save_name, val_clip_save_name, text_save_name =  cbm_utils.get_save_names(args.clip_name, args.backbone,
                                             args.feature_layer, d_val, args.concept_set, "avg", args.activation_dir)
-    s_text = '/data/jong980812/project/Video-CBM/results/ssv2/finetune_internvid_ost_spatio/activation/ssv2_ost_spatio_concepts_ViT-B16.pt'
-    t_text= '/data/jong980812/project/Video-CBM/results/ssv2/finetune_internvid_ost_temporal/activation/ssv2_ost_temporal_concepts_ViT-B16.pt'
+    s_text = '/data/jong980812/project/Video-CBM/results/origin_ssv2/finetune_internvid_ost_spatio/activation/ssv2_ost_spatio_concepts_ViT-B16.pt'
+    t_text= '/data/jong980812/project/Video-CBM/results/origin_ssv2/finetune_internvid_ost_temporal/activation/ssv2_ost_temporal_concepts_ViT-B16.pt'
     if dist.get_rank()==0:
         proj_layers={}
         for j,text_save_name in enumerate([s_text,t_text]):
