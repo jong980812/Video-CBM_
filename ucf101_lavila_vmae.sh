83e3badb66107ee4b68ec691d4e2fa69a6c69720
jong980812 <jong980812@khu.ac.kr>
2024-09-16 17:45:20 +0900

    0916

--- ucf101_lavila_vmae.sh
+++ ucf101_lavila_vmae.sh
@@ -2,20 +2,20 @@
 #SBATCH --job-name example_selection_test
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=12
-#SBATCH --mem-per-gpu=30G
+#SBATCH --mem-per-gpu=50G
 #SBATCH --time 1-00:00:0
 #SBATCH --partition batch_ce_ugrad
 #SBATCH -w moana-y5
-#SBATCH -o /data/jong980812/project/Video-CBM/results/ucf101_internvid_vmae_ucf_finetune/ucf_sub/%A-%x.out
-#SBATCH -e /data/jong980812/project/Video-CBM/results/ucf101_internvid_vmae_ucf_finetune/ucf_sub/%A-%x.err
+#SBATCH -o /data/jong980812/project/Video-CBM/results/ucf101/class_list/%A-%x.out
+#SBATCH -e /data/jong980812/project/Video-CBM/results/ucf101/class_list/%A-%x.err
 
-OUTPUT_DIR='/data/jong980812/project/Video-CBM/results/ucf101_internvid_vmae_ucf_finetune/ucf_sub'
+OUTPUT_DIR='/data/jong980812/project/Video-CBM/results/ucf101/class_list'
 
 
 python /data/jong980812/project/Video-CBM/train_video_cbm.py \
 --data_set UCF101 \
 --nb_classes 101 \
---concept_set /data/jong980812/project/Video-CBM/data/concept_sets/ucf101_subactions.txt \
+--concept_set /data/jong980812/project/Video-CBM/data/ucf101_classes.txt \
 --batch_size 48 \
 --video-anno-path data/video_annotation/ucf101 \
 --data_path /local_datasets/ucf101/videos \
@@ -27,4 +27,6 @@ python /data/jong980812/project/Video-CBM/train_video_cbm.py \
 --backbone vmae_vit_base_patch16_224 \
 --finetune /data/datasets/video_checkpoint/ucf101/ucf_finetune.pth \
 --lavila_ckpt /data/datasets/video_checkpoint/lavila_TSF_B.pth \
---dual_encoder internvid
+--dual_encoder internvid \
+--clip_cutoff 0.1 \
+--interpretability_cutoff 0.1 \
